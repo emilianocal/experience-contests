45484ab9a57b7281a09858bbe32add546df7d102
Emiliano Calvo Alcañiz <ecalvoal@arizona.edu>
2026-02-02 13:24:02 -0700

    create script-01

---
+++ script-01.R
@@ -0,0 +1,17 @@
+#######################################################
+#### Emiliano Calvo Alcaniz ###########################
+#######################################################
+############## Ch 2 ###################################
+####### What is the role of experience ################
+####### in determining contest success? ###############
+#######################################################
+# Jan-2026
+#######################################################
+
+# Setting up stuff (Libraries, etc) ------------------------------------
+# My working directory: 
+# setwd("C:/Users/wjwor/OneDrive/Desktop/gitrepos/experience-contests")
+
+
+# install.packages('dplyr')
+library("dplyr")
